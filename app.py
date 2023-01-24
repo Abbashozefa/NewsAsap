3fc8f0410ef83e77011da78124a899d9705b23c7
Abbashozefa <abbashozefa@gmail.com>
2023-01-24 15:52:20 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask,render_template,request
+
+import requests
+app=Flask(__name__)
+
+
+@app.route("/")
+def welcome():
+    return render_template('index.html')
+
+@app.route("/news",methods=['POST'])
+def news():
+    news=str(request.form['topic'])
+    url = 'https://newsapi.org/v2/everything?q='+news+'&sortBy=popularity&apiKey=e21c28ebd4ff471e970182ebc9da81fd'
+    latest_news = requests.get(url).json()
+    a = latest_news['articles']
+    desc =[]
+    title =[]
+    img =[]
+    for i in range(len(a)):
+          f = a[i]
+          title.append(f['title'])
+          desc.append(f['description'])
+          img.append(f['urlToImage'])
+    
+    
+    return render_template('index.html',mylist = zip(title, desc, img))
+
+
+if __name__  == "__main__":
+    app.run(debug=True)
